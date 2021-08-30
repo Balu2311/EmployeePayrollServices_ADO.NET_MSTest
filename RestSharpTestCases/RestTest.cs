7906bd9057110b08833dcc26eb4f87cb5efc6553
Vallem Balu <vbnreddy5@gmail.com>
2021-08-30 22:09:51 +0530

    Retrive all Employees from Json File Using RESTSHARP_API

---
+++ RestSharpTestCases/RestTest.cs
@@ -0,0 +1,50 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Collections.Generic;
+using System.Net;
+
+namespace RestSharpTestCases
+{
+    [TestClass]
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string salary { get; set; }
+    }
+
+    [TestClass]
+    public class RestTest
+    {
+        RestClient client;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            client = new RestClient("http://localhost:4000");
+        }
+
+        private IRestResponse getEmployeeList()
+        {
+            RestRequest request = new RestRequest("/employee", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+        // Retrive all employees using rest api.
+        [TestMethod]
+        public void OnCallingGETApi_ReturnEmployeeList()
+        {
+            IRestResponse response = getEmployeeList();
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            List<Employee> dataResponse = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
+            Assert.AreEqual(7, dataResponse.Count);
+
+            foreach (Employee e in dataResponse)
+            {
+                System.Console.WriteLine("id: " + e.id + ",Name: " + e.name + ",Salary" + e.salary);
+            }
+        }
+    }
+}
